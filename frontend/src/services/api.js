60b844528bbe5fca2f49e5e38c94e61f9133731d
Mohamed Ismail <mohamedismailibrahim99@gmail.com>
2026-06-26 18:53:32 +0530

    Updated production backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
-  // baseURL: import.meta.env.VITE_API_URL,
+  // baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Add a request interceptor to include the JWT token
